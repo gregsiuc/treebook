a7b8fcc9283bad14e6c7248d4aa0fd7d6017a622
Greg Schloemann <greg.schloemann@gmail.com>
2014-01-08 21:00:43 -0600

    Installed devise, add a user

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Treebook::Application.routes.draw do
+  devise_for :users
   resources :statuses
 
+
+  root to: 'statuses#index'
+  #root to: 'users#sign_up'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
